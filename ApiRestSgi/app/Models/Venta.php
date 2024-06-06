4905aeb32f2496d9dbdd596d5fff006ec06dbee0
Sebastian <menaloaizasebastian@gmail.com>
2024-06-05 19:49:35 -0500

    cambios del projecto

--- ApiRestSgi/app/Models/Venta.php
+++ ApiRestSgi/app/Models/Venta.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Venta extends Model
 {
     use HasFactory;
+    protected $table = 'ventas';
+
+    protected $fillable = [
+        'v_venta',
+        'f_venta',
+        'producto_id',
+        'c_compra'
+    ];
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
 }

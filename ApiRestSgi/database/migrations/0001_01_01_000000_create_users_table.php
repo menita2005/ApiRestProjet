c620d98e758856ec152a89254fc570fd1098d66e
Sebastian <menaloaizasebastian@gmail.com>
2024-06-21 02:28:40 -0500

    ok

--- ApiRestSgi/database/migrations/0001_01_01_000000_create_users_table.php
+++ ApiRestSgi/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('Encargado'); // Añadir columna role
+            $table->string('usertype')->default('Encargado'); // Añadir columna role
             $table->boolean('status')->default(true); // Añadir columna status
             $table->rememberToken();
             $table->timestamps();
